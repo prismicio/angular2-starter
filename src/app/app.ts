87928e6c64ae04f12271e475a42a46463d6f86ba
Arnaud lewis <alw@wengularity.com>
2016-10-27 18:31:09 +0200

    query repo for onboarding validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,16 @@
 import {Component} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Inject} from '@angular/core';
 
 @Component({
   selector   : 'app',
   templateUrl: './app.html',
 })
 export class AppComponent {
+  constructor(public http: Http, @Inject('PrismicEndpoint') private endpoint: string) {}
+
+  ngOnInit() {
+    const repoEndpoint = this.endpoint.replace("/api", "");
+    this.http.post(repoEndpoint + '/app/settings/onboarding/run', {}, new Headers({"Content-Type": "application/x-www-form-urlencoded", 'Access-Control-Allow-Origin': '*'})).subscribe(res=>null,error => null)
+  }
 }
