f24cc543029a125c2624f7524205868d6fbd63eb
Arnaud lewis <arnaud.lewis@gmail.com>
2017-05-07 14:34:14 +0200

    highlight js

--- src/app/modules/help/help.component.ts
+++ src/app/modules/help/help.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 import { PrismicService } from '../../prismic/prismic.service';
+import { HighlightJsService } from 'angular2-highlight-js';
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.css']
 })
-export class HelpComponent {
+export class HelpComponent implements AfterViewInit {
   repositoryInfos: Object;
 
-  constructor(prismic: PrismicService) {
+  constructor(private prismic: PrismicService, private el: ElementRef, private highlightService : HighlightJsService) {
     prismic.validateOnboarding();
     this.repositoryInfos = prismic.getRepositoryInfos();
+  }
+
+  ngAfterViewInit() {        
+    const codeElements = this.el.nativeElement.querySelectorAll('pre code');
+    if(codeElements) {
+      codeElements.forEach(code => {
+        this.highlightService.highlight(code);
+      });
+    }
   }
 
   goToAnchor(anchor: string): void {
--- src/app/modules/help/help.module.ts
+++ src/app/modules/help/help.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'; // required for ngIf templating property
 import { RouterModule } from '@angular/router';
+import { HighlightJsModule, HighlightJsService } from 'angular2-highlight-js';
+
 import { HelpComponent } from './help.component';
 import { PrismicService } from '../../prismic/prismic.service';
 import { routes } from '../../routes';
 
 @NgModule({
   declarations: [HelpComponent],
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes), HighlightJsModule],
   exports: [HelpComponent],
-  providers: [PrismicService]
+  providers: [PrismicService, HighlightJsService]
 })
 export class HelpModule { }
